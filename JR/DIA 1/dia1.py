7fc2615439182a2bb798a8207891002a13134bec
JARU0624 <161090350+JARU0624@users.noreply.github.com>
2024-02-23 18:24:54 -0500

    Add files via upload

---
+++ JR/DIA 1/dia1.py
@@ -0,0 +1,23 @@
+#Ejercicio 1
+
+"""nombre = input("¿Cual es su nombre? \n")
+print("Su nombre es" + nombre)"""
+
+nombre = input("Ingrese su nombre \n")
+
+#Ejercicio 2
+
+Edad = int(input("Indique su edad: \n"))
+Altura = float (input("Indique su altura: \n"))
+Peso = float(input("¿Cual es su peso? \n"))
+
+
+print("Su edad es" , Edad , "años" , "Su altura es" ,Altura , "cm" , "y su peso es de" , Peso , "kg")
+
+
+#Ejercicio 3
+
+
+
+
+

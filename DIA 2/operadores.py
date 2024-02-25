4f5d811dd44e7ca0cff266a1a72a787876df1b5a
JARU0624 <161090350+JARU0624@users.noreply.github.com>
2024-02-25 18:01:50 -0500

    Update operadores.py

--- DIA 2/operadores.py
+++ DIA 2/operadores.py
@@ -73,4 +73,29 @@ print(f"""
       de{numeroUno} % {numeroDos} 
       es: {result}""")
 
+#Ejercicios (escribe un programa que solicite al usuario ingresar dos numeros enteros y realice las siguierntes operaciones)
+# Usar solo 2 variables
+
+
+numeroUno = int(input("indique un numero entero: \n" ))
+numeroDos = int(input("Indique un segundo numero entero: \n"))
+print("Los numeros que usted indico son:" , numeroUno , "y", numeroDos)
+
+#Realice una suma de los dos numeros
+result = numeroUno + numeroDos
+print("La suma de los valores es:" , result)
+
+#Realice una resta del segundo numero al primero
+result = numeroDos - numeroUno
+print("la resta del segundo numero al primero es:" , result)
+
+#Realice una multiplicacion de los numeros
+result = numeroUno * numeroDos
+print("la multiplicacion de ambos nu+meros es:" , result)
+
+#Realice una divicion del primer numero entre el segundo
+result = numeroUno / numeroDos
+print("La division entre los valores dados es:" , result)
+
+#Realice una division entera del segundo numero entre el primero
 
